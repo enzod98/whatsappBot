df766798cd0017e636677a307fbefa5ee1df34a6
Enzo <enzod98@outlook.com>
2020-07-16 15:55:37 -0400

    Agregada configuración express

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
+require('./config/config');
+
+const express = require('express');
+const fs = require('fs');
 
 const { Client } = require('whatsapp-web.js');
-const fs = require('fs');
 const client = require('../controllers/funciones');
+
+const app = express();
 
 client.initialize();
 
+
+app.listen(process.env.PORT, () => {
+    console.log('Esuchando el puerto: ', process.env.PORT);
+})
